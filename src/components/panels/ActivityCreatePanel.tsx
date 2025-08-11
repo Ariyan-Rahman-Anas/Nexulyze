85707550f3aae3ded897e5f1083db302879d2ff4
Your Personal Name <your.personal@email.com>
2025-08-11 18:01:31 +0600

    increased the width of drawer

--- src/components/panels/ActivityCreatePanel.tsx
+++ src/components/panels/ActivityCreatePanel.tsx
@@ -14,7 +14,7 @@ export function ActivityCreatePanel({
 }: ActivityCreatePanelProps) {
   return (
     <Drawer open={open} onOpenChange={onOpenChange} direction="right">
-        <DrawerContent className="h-full max-w-md ml-auto mr-0 mt-0 rounded-l-lg rounded-r-none border-l border-t-0 border-r-0 border-b-0 fixed right-0 top-0 data-[state=open]:animate-slide-in-from-right data-[state=closed]:animate-slide-out-to-right">
+        <DrawerContent className="h-full max-w-lg ml-auto mr-0 mt-0 rounded-l-lg rounded-r-none border-l border-t-0 border-r-0 border-b-0 fixed right-0 top-0 data-[state=open]:animate-slide-in-from-right data-[state=closed]:animate-slide-out-to-right">
           <div className="p-6 pt-0 overflow-y-auto h-full">
             <DrawerHeader className="px-0">
             </DrawerHeader>
