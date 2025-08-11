663b675e0b8d7cfe07ba202bb491f32a24556198
Your Personal Name <your.personal@email.com>
2025-08-11 17:55:47 +0600

    changed folder name

--- src/components/panels/ActivityCreatePanel.tsx
+++ src/components/panels/ActivityCreatePanel.tsx
@@ -3,15 +3,15 @@
 import { Drawer, DrawerContent, DrawerHeader } from "@/components/ui/drawer";
 import ActivityCreateForm from "../forms/ActivityCreateForm";
 
-interface CreateActivityPanelProps {
+interface ActivityCreatePanelProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
 }
 
-export function CreateActivityPanel({
+export function ActivityCreatePanel({
   open,
   onOpenChange,
-}: CreateActivityPanelProps) {
+}: ActivityCreatePanelProps) {
   return (
     <Drawer open={open} onOpenChange={onOpenChange} direction="right">
         <DrawerContent className="h-full max-w-md ml-auto mr-0 mt-0 rounded-l-lg rounded-r-none border-l border-t-0 border-r-0 border-b-0 fixed right-0 top-0 data-[state=open]:animate-slide-in-from-right data-[state=closed]:animate-slide-out-to-right">
